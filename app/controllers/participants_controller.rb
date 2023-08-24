9a58b75e9a19e3fdd4f9b1651819c74a2f266bca
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2023-08-24 15:18:56 +0300

    Add id to divs in Game Show, update Turbo logic

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -5,23 +5,40 @@ class ParticipantsController < ApplicationController # :nodoc:
 
   def create
     authorize @game, :full?
-    @participant = @game.participants.create! params.required(:participant).permit(:game_id, :user_id, :name, :number)
+    @participant = @game.participants.new(participant_params)
 
     # if current_user && current_user.number == @participant.number && current_user&.avatar&.attached?
     #   @participant.avatar.attach(current_user.avatar_blob)
     # end
     # redirect_to @game
-    @participants = @game.participants.includes(user: :avatar_attachment)
 
-    render turbo_stream:
-    turbo_stream.replace('participants',
-                         partial: 'participants/participant',
-                         locals: { game: @game, participants: @participants })
+    respond_to do |format|
+      if @participant.save
+        format.turbo_stream do
+          if @participant.user.present? || !policy(@game).full?
+            redirect_to @game
+          else
+            render turbo_stream:
+            turbo_stream.append(:participants,
+                                partial: 'participants/participant',
+                                locals: { game: @game, participant: @participant })
+          end
+        end
+        format.json { render :show, status: :created, location: @game }
+      else
+        format.html { redirect_to @game, status: :unprocessable_entity, alert: "Can't join this game, number and username can't be blank" }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
   def set_game
     @game = Game.find(params[:id])
+  end
+
+  def participant_params
+    params.required(:participant).permit(:game_id, :user_id, :name, :number)
   end
 end
