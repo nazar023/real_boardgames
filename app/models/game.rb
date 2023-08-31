f52897094f8e1054f3457fe3f69328b7a21c779a
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2023-08-31 17:38:40 +0300

    Fix game/user destroying

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,8 +6,8 @@ class Game < ApplicationRecord # :nodoc:
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :winner, class_name: 'Participant', foreign_key: 'winner_id', optional: true
 
-  has_many :participants, dependent: :destroy
-  has_many :game_invites, dependent: :destroy
+  has_many :participants, dependent: :delete_all
+  has_many :game_invites, dependent: :delete_all
 
   scope :with_participants, -> { includes(:participants) }
 end
