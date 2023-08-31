780ea3199a8a3286da9772e8ab32580b7af07bae
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2023-08-31 16:57:12 +0300

    Change json column to jsonb in Notification table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_122844) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_135420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,7 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_122844) do
     t.string "recipient_type", null: false
     t.bigint "recipient_id", null: false
     t.string "type", null: false
-    t.json "params"
+    t.jsonb "params"
     t.datetime "read_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
