e4caed3158abed480a1738aa6256b499e73a824a
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2023-09-11 22:54:09 +0300

    Covered GameInvite Notitfication with test

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Notification, type: :model do
+RSpec.describe 'Notification', type: :model do
   it 'deliver notification to creator' do
     game = create(:game)
     creator = game.creator
-    create(:participant, game: game)
-    create(:participant, game: game)
-    create(:participant, game: game)
+    create(:participant, game_id: game.id)
+    create(:participant, game_id: game.id)
+    create(:participant, game_id: game.id)
     expect(creator.notifications.count).to be(3)
+  end
+
+  it 'deliver notification to invited preson' do
+    game = create(:game)
+    user = create(:user)
+    creator = game.creator
+    expect { creator.send_game_invite(user, game) }.to(
+      change { user.notifications.count }.from(0).to(1)
+    )
   end
 end
