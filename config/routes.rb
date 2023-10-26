7712310f8a447a8f55def8adfd7def3d9a46d113
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2023-10-26 15:30:31 +0300

    Move to Rails 7.1.1 and Delete devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-                                  registrations: 'users/registrations',
-                                  sessions: 'users/sessions'
-                                }
+  # devise_for :users, controllers: {
+  #                                 registrations: 'users/registrations',
+  #                                 sessions: 'users/sessions'
+  #                               }
+
+  resources :session
+  resources :registration
+  resources :password
+  resources :password_reset
+
 
   resources :games
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_22_203219) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_121725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,11 +107,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_203219) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
@@ -119,9 +116,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_203219) do
     t.integer "games_count", default: 0
     t.integer "wins_count", default: 0
     t.integer "status", default: 0
-    t.datetime "last_time_online_at", precision: nil, default: "2023-10-22 20:30:09"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.datetime "last_time_online_at", precision: nil, default: "2023-10-26 12:27:56"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
